201106ca8cc682f736ef4219ae576b2757da664c
viniciusprogr <vnsrodrigues10@gmail.com>
2025-05-21 13:51:20 -0300

    gerando projeto

---
+++ src/main/java/com/agenda/Timebook/TimebookApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.Timebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenda/Timebook/TimebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.Timebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
